b161fd0aac971725aa9b905097c3e32910b1028f
Daniel <prayson.wilfred.daniel@if.dk>
2018-07-11 05:33:00 +0200

    Changed name from TrustPilot to tpreviews, added reading companies name from csv

---
+++ test.py
@@ -0,0 +1,21 @@
+import numpy as np
+from TrustPilot import GetReviews
+
+comp_dict = {'Mate': '587381110000ff00059a7856',
+             'Spigo': '46d2b21a0000640005009a08'}
+
+# Initiating with dictionary
+#t = GetReviews(comp_dict)
+
+#Using Default values
+#t.save_data()
+
+# Dictionary from Data 
+lines = np.genfromtxt('companies_ids.csv', delimiter=',',
+                      dtype=str, skip_header=1)
+csv_dict = {key: item for key, item in lines}
+
+d = GetReviews(csv_dict)
+d.gather_data('no') # Get Norwegian Reviews
+d.save_data(file_name='NoTrustPilotData')
+
